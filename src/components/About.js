498b137f2e02d6a61034e653f9382291de9c8ddb
Samson Mubeya <rasspartah@gmail.com>
2024-04-29 16:02:09 +0300

    added the Links component to display links

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,18 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About({bio,links}) {
+  function dontRender(){
+    if (!bio){ return ""}
+    else {return <p>{bio}</p>}
+  }
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {dontRender()}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <Links github={links.github} linkedin={links.linkedin} />
     </div>
   );
 }
